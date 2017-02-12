e0e554ac1690dcfd71199049acc3b0c191961f46
Ezequiel <ezequiel.d.orozco@gmail.com>
2017-02-12 16:05:12 +0100

    Initial commit

--- examples/bar.class.php
+++
@@ -1,7 +0,0 @@
-<?php 
-class Bar {
-    public function sayHi(){
-        echo 'Hellow motherfucker';
-    }
-}    
-    
--- examples/foo.class.php
+++
@@ -1,7 +0,0 @@
-<?php 
-class Foo {
-    public function goSleep(){
-        echo 'Good Night motherfucker';
-    }
-}    
-    
